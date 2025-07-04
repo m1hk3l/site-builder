e15f3c059ea72f9eecd08515a4554e999100bbb7
m1hk3l <mihkel.kagovere@gmail.com>
2025-07-04 19:02:35 +0300

    FIX: homepage is served from wagtail, customusermodel implemented

--- cplans/cplans/settings/base.py
+++ cplans/cplans/settings/base.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
     # custom additions
     "accounts.apps.AccountsConfig",
+    "pages.apps.PagesConfig",
 ]
 
 MIDDLEWARE = [
--- cplans/home/models.py
+++ cplans/home/models.py
@@ -1,7 +1,32 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.images.models import Image
+from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    blogger_photo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text="Photo of the blogger for the landing page."
+    )
+    main_heading = models.CharField(
+        max_length=255,
+        help_text="Main heading (h1) for the landing page."
+    )
+    landing_text = RichTextField(
+        features=None,  # None means all features enabled
+        help_text="Landing page text with full rich text support.",
+        default=""
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('blogger_photo'),
+        FieldPanel('main_heading'),
+        FieldPanel('landing_text'),
+    ]
